4abd3d050355997c34605392ef28b47169e2c230
Brendon <brendon.stanley@rams.colostate.edu>
2020-04-08 22:37:39 -0600

    env var change

--- DSM/Docker/Data_Entry_App/models.py
+++ DSM/Docker/Data_Entry_App/models.py
@@ -3,11 +3,11 @@ import os
 
 db_name = os.environ.get("db_name", "IoT")
 db_host = os.environ.get("db_host", "db")
-db_post = int(os.environ.get("db_port", "3306"))
+db_port = int(os.environ.get("db_port", "3306"))
 db_user = os.environ.get("db_user", "root")
-db_paswd = os.environ.get("db_password", "example")
+db_password = os.environ.get("db_password", "example")
 
-db = peewee.MySQLDatabase(db_name, host=db_host, port=db_post, user=db_user, passwd=db_paswd)
+db = peewee.MySQLDatabase(db_name, host=db_host, port=db_port, user=db_user, passwd=db_password)
 
 
 class BaseModel(peewee.Model):
