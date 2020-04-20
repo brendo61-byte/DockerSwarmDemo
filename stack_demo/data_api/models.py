e391d75febe1a12c8276a3ba7496c64a33e1d890
Brendon <brendon.stanley@rams.colostate.edu>
2020-04-20 12:49:55 -0600

    docker stack mk2

---
+++ stack_demo/data_api/models.py
@@ -0,0 +1,22 @@
+import peewee
+import os
+
+db_name = os.environ.get("db_name")
+db_host = os.environ.get("db_host")
+db_port = int(os.environ.get("db_port"))
+db_user = os.environ.get("db_user")
+db_password = os.environ.get("db_password")
+
+db = peewee.MySQLDatabase(db_name, host=db_host, port=db_port, user=db_user, passwd=db_password)
+
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
+
+
+class DataEntry(BaseModel):
+    value = peewee.FloatField()
+    timeStamp = peewee.FloatField()
+    units = peewee.CharField(max_length=10)
+    readingType = peewee.CharField(max_length=25)
